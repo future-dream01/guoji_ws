dab37f18aa8b9608c55cae613fef638b8f89dd56
朝闻道 <138562772+future-dream01@users.noreply.github.com>
2024-04-12 11:42:58 +0800

    完善了主函数节点和舵机投递节点
    为yolox识别增加了指令发布者和数据订阅者节点，同时完善了舵机的投递逻辑

--- src/servo/scripts/servo.py
+++ src/servo/scripts/servo.py
@@ -5,20 +5,39 @@ import rospy
 from std_msgs.msg import UInt8
 
 
+x=0     # 0是初值，1代表投递第一个货物，2代表第二个，3代表第三个
+y=0     # 0是初值，1代表一次投递完成
+
+
 class Subscriber_servo_node:
     def __init__(self):
         rospy.init_node("node3",anonymous=True)
-        self.sub=rospy.Subscriber("servo_move",UInt8,self.callback)
+        self.sub=rospy.Subscriber("servo_action",UInt8,self.callback)
+        self.rate=rospy.Rate(10)        # 一定频率，防止过多占用CPU资源
 
     def callback(self,msg):
-        rospy.loginfo(rospy.get_caller_id()+"Now the target is %s",msg.data)
-        # 接下来进行舵机程序
+        global x
+        #rospy.loginfo(rospy.get_caller_id()+"Now the target is %s",msg.data)
+        x=msg.data
 
-    def start_listening(self):
-        rospy.spin()
+def delivery():
+    # 舵机指定角度的旋转，完成投递
+
+    # 先顺时针转30度，
+    # 延时1秒
+    # 逆时针转30度，完成一次投递
+    y=1
 
 if __name__=='__main__':
     servo_node=Subscriber_servo_node()
-    while True:
-        pass
-    #servo_node.start_listening()
+    while not rospy.is_shutdown():
+        if x==1 and y==0:       # 第一个货物投递
+            delivery()
+            y=0
+        if x==2 and y==0:       # 第二个货物投递
+            delivery()
+            y=0
+        if x==3 and y==0:       # 第三个货物投递
+            delivery()
+            y==0
+        servo_node.rate.sleep() # 休眠，防止过多占用CPU资源
