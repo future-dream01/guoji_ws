ee078cb56c37170df3c0faa318d2899533dc93bf
GalahadX <2370703976@qq.com>
2024-04-15 21:41:33 +0800

    识别节点修改0415
    搭建了识别节点的基本框架，我认为它能跑，但是ROS是不是这样认为就不知道了

--- src/shibie/identify_control.py
+++ src/shibie/identify_control.py
@@ -3,28 +3,45 @@
 import rospy
 import time
 from shibie.srv import Yolox_action, Yolox_data
-from std_msgs.msg import String
+action_judge = True
+
+def identify_data_callback(msg):
+    # 接受并输出识别结果
+    rospy.loginfo(f"Taregt:{msg.target} \n X_postion:{msg.x_p} \n Y_postion:{msg.y_p}")
 
 
-def target_identify():
-    """向服务器发出信号并接收识别结果"""
+def identify_call():
+    # 向服务器发出信号并接收识别结果
+
+    """
+    此处定义全局变量action_bool，用于控制接收循环是否停止，
+    当主程序判定循环应当停止时，更改action_bool为False，则该节点停止订阅yolox_back的内容，并向identify_node发出停止的信号。
+    """
+    global action_judge
+
     # ROS节点初始化
     rospy.init_node("identify_control")
+    #创建名为identify_control的Publisher，发布名为yolox_call的topic, 消息类型为Yolox_action，队列长度为5
+    identify_control = rospy.Publisher('yolox_call', Yolox_action, queue_size = 5)
+    control_cmd = Yolox_action()
+    control_cmd.action = 1
+    identify_control.publish(control_cmd)
 
-    #发现identify_server服务，创建客户端，连接服务
-    rospy.wait_for_service('identify_server')
-    try:
-        add_ide = rospy.ServiceProxy('identify_server', Identify)
-        # 请求服务调用，输入请求数据
-        response = add_ide("1")
-        return response
+    t1 = time.perf_counter()
+    while action_judge:
+        rospy.Subscriber('yolox_back', Yolox_data, identify_data_callback)
+        t2 = time.perf_counter()
+        if (t1 - t2) == 60:
+            action_judge = False
+        rospy.spin()
 
-    except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
+    # 跳出循环后，发布停止消息
+    control_cmd.action = 0
+    identify_control.publish(control_cmd)
 
 
 if __name__ == "__main__":
     # 服务器调用并显示结果
     # t1 = time.perf_counter()
-    response = target_identify()
+    identify_call()
     # t2 = time.perf_counter()
