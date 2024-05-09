963ffec16225942c0aca2d7fc444d1972fb872b9
朝闻道 <138562772+future-dream01@users.noreply.github.com>
2024-05-09 15:56:41 +0800

    再次完善了无人机的任务逻辑，增加了着陆部分函数，修补了任务开始阶段的bug，程序已可用

--- src/main/scripts/main1.py
+++ src/main/scripts/main1.py
@@ -24,39 +24,58 @@ class MainNode():
         # 属性
         self.x=self.y=self.z=0                                                                              # 初始化位置x，y
         self.x_p=self.y_p=self.obj=0                                                                        # 初始化目标位置偏移x_p、y_p、物体类型
-        self.state=False                                                                                    # 无人机是否已起飞
+        self.takeoff_state=False                                                                            # 无人机是否起飞
         self.rate=rospy.Rate(10)                                                                            # 频率
+        self.armed_state=False                                                                              # 无人机是否解锁
+        self.is_landing=False
         # 话题
         self.servo_pub=rospy.Publisher("servo_action",UInt8,queue_size=10)                                  # 舵机发布者节点
         self.rplidar_sub=rospy.Subscriber("/slam_out_pose",PoseStamped,self.rplidar_callback)               # 雷达订阅者节点 
-        self.yolox_pub=rospy.Publisher("yolox_action",Yolox_action,queue_size=10)                                  # 识别指令发布者
-        self.yolox_sub=rospy.Subscriber("yolox_data",Yolox_data,self.yolox_callback)                       # 识别结果订阅者
+        self.yolox_pub=rospy.Publisher("yolox_action",Yolox_action,queue_size=10)                           # 识别指令发布者
+        self.yolox_sub=rospy.Subscriber("yolox_data",Yolox_data,self.yolox_callback)                        # 识别结果订阅者
         self.own_position_pub=rospy.Publisher('/mavros/vision_pose/pose',PoseStamped,queue_size=10)         # 当前位置发布者
         self.aim_position_pub=rospy.Publisher('/mavros/setpoint_position/local',PoseStamped,queue_size=10)  # 飞行目标点发布者
         self.shibie_move_pub=rospy.Publisher('/mavros/setpoint_position/local',PoseStamped,queue_size=10)   # 识别到目标之后调整位置
+        self.own_altitude_sub=rospy.Subscriber('/mavros/local_position/pose',PoseStamped,self.altitude_callback)
         self.state_sub=rospy.Subscriber('/mavros/state',State,self.state_callback)                          # 无人机状态订阅者
         # 服务
-        #rospy.wait_for_service('/mavros/set_mode')                                                          # 确保服务可用
         self.set_mode_client = rospy.ServiceProxy('/mavros/set_mode', SetMode)                              # 飞行模式切换服务代理
 
-    def state_callback(self,msg):                                                                           # 监听无人机是否已起飞
+    # 解锁状态监听函数
+    def state_callback(self,msg):                                                                           
         if msg.armed:
-            rospy.loginfo("无人机已正常起飞")
-            self.state=True
+            rospy.loginfo("无人机已解锁")
+            self.armed_state=True
         else:
+            self.armed_state=False
+            rospy.loginfo("无人机未解锁")
+
+    # 起飞状态监听函数
+    def altitude_callback(self,msg):
+        altitude=msg.pose.positoion.z
+        if (altitude>=0.5):
+            self.arm_takeoff=True
+            rospy.loginfo("无人机已起飞")
+        else :
+            self.arm_takeoff=False
             rospy.loginfo("无人机未起飞")
 
-    def set_mode(self,mode):                                                                                # 设置无人机飞行模式
+    # 设置无人机飞行模式
+    def set_mode(self,mode):                                                                               
+        rospy.wait_for_service('/mavros/set_mode')
         try:
             response=self.set_mode_client(custom_mode=mode)
             if response.mode_sent:
-                rospy.loginfo("Mode change to OFFBOARD successful")
+                rospy.loginfo(f"Mode change to {mode} successful")
+                if mode =="AUTO.LAND":
+                    self.is_landing==True
             else:
                 rospy.loginfo("Mode change failed")
         except rospy.ServiceException as e:
             rospy.logerr("Service call failed: %s" % e)
-            
-    def shibie_pub(self,a):                                        # 识别状态发布函数
+
+    # 识别状态发布函数
+    def shibie_pub(self,a):                                        
         global tim
         act=Yolox_action()
         tim=time.time()                                           # 记录当前时间
@@ -64,21 +83,21 @@ class MainNode():
             act.action=1 
             for i in range(0,10):
                 self.yolox_pub.publish(act)
-                #rospy.loginfo(act)
                 self.rate.sleep()
-                
         if a==2:                                                   # 2代表结束识别
             act.action=2 
             for i in range(0,10):
                 self.yolox_pub.publish(act)
                 self.rate.sleep()
 
-    def rplidar_callback(self,msg):                                # 激光雷达位姿数据订阅函数
-        self.x = msg.pose.position.x                               # 无人机当前的x位置
-        self.y = msg.pose.position.y                               # 无人机当前的y位置
-        self.own_position_pub.publish(msg)                         # 向飞控发布坐标信息
+    # 激光雷达位姿数据订阅函数+坐标变换
+    def rplidar_callback(self,msg):                                
+        self.x = -msg.pose.position.y                              # 无人机当前的x位置
+        self.y = msg.pose.position.x                               # 无人机当前的y位置
+        self.own_position_pub.publish(msg)                         # 向飞控发布当前无人机自身坐标信息
 
-    def shibie_move_fix(self,z):                                   # 发现目标之后开始调整定位，需给定高度
+    # 发现目标之后开始调整定位，需给定高度
+    def shibie_move_fix(self,z):                                   
         position=PoseStamped()
         while not ((-70<=(self.x_p)<=70) and(70<=(self.y_p)<=70) ):
             position.header.stamp=rospy.Time.now()
@@ -88,8 +107,8 @@ class MainNode():
             position.pose.position.z=z                             # 目标点的z坐标
             self.aim_position_pub.publish(position)
 
-        
-    def send_aim_posion(self,x,y,z):                               # 发送目标点位置信息
+    # 发送目标点位置信息
+    def send_aim_posion(self,x,y,z):                               
         position=PoseStamped()
         while not ((-0.1<=(self.x-x)<=0.1) and(-0.1<=(self.y-y)<=0.1)) :
             position.header.stamp=rospy.Time.now()
@@ -99,15 +118,24 @@ class MainNode():
             position.pose.position.z=z                             # 目标点的z坐标
             self.aim_position_pub.publish(position)
 
-    def yolox_callback(self,msg):                                  # 识别数据订阅函数
+    # 识别数据订阅函数
+    def yolox_callback(self,msg):                                  
         global tim
         tim2=time.time()
-        fps=1/(tim2-tim)                                         # 计算fps
+        fps=1/(tim2-tim)                                      # 计算fps
         obj=msg.target                                        # 识别出的物体类别
         x_p=msg.x_p                                           # 物体的x偏移量
         y_p=msg.y_p                                           # 物体的y偏移量
         tim =tim2
         rospy.loginfo(f"obj: {obj} \n x_p:{x_p} \n y_p:{y_p} \n fps: {fps}")   # 日志
+
+    # 着陆函数
+    def land(self):
+        while not self.is_landing:
+            self.set_mode("AUTO.LAND")
+        while self.armed_state:
+            rospy.loginfo("the plane is landing……")
+        rospy.loginfo("the plane landed and poweroffed successfully")
 
 # 信号灯类
 #class Light():
@@ -130,7 +158,8 @@ class UART(serial.Serial):
     def __init__(self, port, baudrate,timeout):
         super(UART, self).__init__(port=port, baudrate=baudrate, timeout=timeout)
 
-    def servo_start(self,a):                # 舵机开始运动,a=1:投第一个货物；a=2:投第二个货物;a=3:投第三个货物
+    # 舵机运动函数（a=1投第一个货物；a=2投第二个货物;a=3投第三个货物）
+    def servo_start(self,a):                
         for i in range(0,10): 
             command = a.to_bytes(1, 'little')             
             self.write(command)
@@ -148,27 +177,30 @@ def shibie_toudi(main_node,servo):
         pass
     else:
         i+=1
-        
-# 主函数
 
+# 主函数
 def main():
-    b=0
     main_node=MainNode()
     servo=UART(port, baudrate,timeout)
     #light=Light()
-    main_node.shibie_pub(1)
+    #main_node.shibie_pub(1)
     #rospy.sleep(15)
-    servo.servo_start(2)
+    #servo.servo_start(2)
     while not rospy.is_shutdown(): 
-        pass
-        # if main_node.state:                                                     # 确定无人机是起飞状态
-        #     main_node.set_mode("OFFBOARD")                                      # 将无人机飞行模式切换到OFFBOARD，由ros程序控制
-        #     time.sleep(1)
-        #     main_node.send_aim_posion(position[i][0],position[i][1],1)          # 前往指定点
-        #     main_node.shibie_pub(1)                                             # 开始识别
-        #     time.sleep(4)                                                       # 等待识别开始出结果
-        #     shibie_toudi(main_node,servo)                                       # 投递函数
-   # main_node.shibie_pub(2)
+        if (main_node.armed_state) and (main_node.takeoff_state):                            # 确定无人机是否解锁、起飞
+            #main_node.set_mode("OFFBOARD") 
+            #main_node.send_aim_posion(0,0,1)
+            #rospy.sleep(10)
+            #while not rospy.is_shutdown():
+                #main_node.set_mode("OFFBOARD")                                      # 将无人机飞行模式切换到OFFBOARD，由ros程序控制
+                #main_node.send_aim_posion(position[i][0],position[i][1],1)          # 前往指定点
+            main_node.send_aim_posion(2,2,1)
+            main_node.land()
+            break
+                #main_node.shibie_pub(1)                                             # 开始识别
+                #time.sleep(4)                                                       # 等待识别开始出结果
+                #shibie_toudi(main_node,servo)                                       # 投递函数
+    #main_node.shibie_pub(2)
 
 if __name__=='__main__':
     main()
